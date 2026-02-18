bb658ad20e22855a857dc39fadf843238fa66898
nutriwisepnh <83652615+nutriwisepnh@users.noreply.github.com>
2026-02-17 21:22:40 -0500

    Update tags.js

--- app/data/tags.js
+++ app/data/tags.js
@@ -2,9 +2,9 @@ export const initialTags = [
   "vis",
   "vezelrijk",
   "eiwitrijk",
-  "vegetarisch"
-  "cholesterol +"
-  "cholesterol -"
-  "Afvallen"
-  "havermout"
+  "vegetarisch",
+  "cholesterol +",
+  "cholesterol -",
+  "Afvallen",
+  "havermout",
 ];
